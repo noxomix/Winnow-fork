84e49347e0d97f4f46e32379ac00c9e8300d3fa8
itsaryanchauhan <itsaryanchauhan1@gmail.com>
2026-03-13 16:38:03 +0530

    chore: release v0.2.7

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from typing import Optional
 from fastapi.responses import RedirectResponse
 
 
-app = FastAPI(title="Winnow API", version="0.2.6")
+app = FastAPI(title="Winnow API", version="0.2.7")
 
 
 @app.get("/")
